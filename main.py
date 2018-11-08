92025969153b7bcdef24159c32752aca538f2325
dickersonsteam <37583333+dickersonsteam@users.noreply.github.com>
2018-11-08 12:06:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 # main.py
-# CircuitPython Blink
+# blink built-in and user-added LED
 
+import board
 import time
 from digitalio import DigitalInOut, Direction
-import board
 
-# define constants
-delayTime = 0.5
+# create IO objects
+# be sure to replace D13 and D2
+# with pins appropriate to your board
+led = DigitalInOut(board.D13)  # built-in LED
+myLed = DigitalInOut(board.D2)  # led added by user in circuit
 
-led = DigitalInOut(board.D6)
+# initialize pins as outputs
 led.direction = Direction.OUTPUT
+myLed.direction = Direction.OUTPUT
+
+# initialize LED states
+led.value = True
+myLed.value = True
 
 while True:
-    led.value = False
-    time.sleep(delayTime) # delay half second
-    led.value = True
-    time.sleep(delayTime)
+    led.value = not led.value
+    myLed.value = not myLed.value
+    
+    time.sleep(1)
+    
